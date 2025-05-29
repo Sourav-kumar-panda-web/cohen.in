3bd6e8b88abdb914269b9d64b46b7b5123c2d287
sourav panda <129955183+Sourav-kumar-panda-web@users.noreply.github.com>
2025-05-29 18:33:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -292,7 +292,8 @@ app.get('/logout', (req, res) => {
 });
 
 // ---------- START SERVER ----------
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
